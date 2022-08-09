4bdc3ff33d2aae1d6b34681d2d1a35b2533922d0
harkiramadhan <harkiramadhan@gmail.com>
2022-08-09 09:54:29 +0700

    updates

--- application/controllers/Orders.php
+++ application/controllers/Orders.php
@@ -11,11 +11,21 @@ class Orders extends CI_Controller{
     }
 
     function detail($id){
+        $orders = $this->db->select('o.*, s.sumber, p.nama nama_pic, c.nama nama_customer, c.nohp, c.instansi, ti.instansi jenis_instansi, ds.status deal_status, os.keterangan order_status')
+                            ->from('orders o')
+                            ->join('sumber s', 'o.sumber_id = s.id')
+                            ->join('pic p', 'o.pic_id = p.id')
+                            ->join('customer c', 'o.customer_id = c.id')
+                            ->join('tipe_instansi ti', 'c.instansi_id = ti.id')
+                            ->join('deal_status ds', 'o.deal_status_id = ds.id')
+                            ->join('order_status os', 'o.order_status_id = os.id')
+                            ->where('o.id', $id)->get()->row();
         $datas = [
             'title' => 'Detail Order',
             'title_desc' => 'Overview dan summary penjualan',
             'page' => 'pages/ordersDetail',
-            'backUrl' => 'crm'
+            'backUrl' => 'crm',
+            'orders' => $orders
         ];
         $this->load->view('template2', $datas);
     }
--- application/views/pages/ordersDetail.php
+++ application/views/pages/ordersDetail.php
@@ -1,16 +1,79 @@
 <div class="content d-flex flex-column flex-column-fluid" id="kt_content">
     <!--begin::Container-->
     <div class="container-xxl" id="kt_content_container">
+        <!--begin::Details-->
+        <div class="d-flex flex-wrap flex-sm-nowrap">
+            <!--begin::Info-->
+            <div class="row flex-grow-1">
+                <!--begin::Title-->
+                <div class="col-lg-7 justify-content-between align-items-start flex-wrap mb-2">
+                    <!--begin::User-->
+                    <div class="d-flex flex-column">
+                        <!--begin::Name-->
+                        <div class="d-flex align-items-center mb-2">
+                            <h1 class="text-gray-900 fs-1 fw-bolder me-1"><?= $orders->kode_order ?></h1>
+                            <a href="#">
+                                <!--begin::Svg Icon | path: icons/duotune/general/gen026.svg-->
+                                <span class="svg-icon svg-icon-1 svg-icon-primary ">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24">
+                                        <path d="M10.0813 3.7242C10.8849 2.16438 13.1151 2.16438 13.9187 3.7242V3.7242C14.4016 4.66147 15.4909 5.1127 16.4951 4.79139V4.79139C18.1663 4.25668 19.7433 5.83365 19.2086 7.50485V7.50485C18.8873 8.50905 19.3385 9.59842 20.2758 10.0813V10.0813C21.8356 10.8849 21.8356 13.1151 20.2758 13.9187V13.9187C19.3385 14.4016 18.8873 15.491 19.2086 16.4951V16.4951C19.7433 18.1663 18.1663 19.7433 16.4951 19.2086V19.2086C15.491 18.8873 14.4016 19.3385 13.9187 20.2758V20.2758C13.1151 21.8356 10.8849 21.8356 10.0813 20.2758V20.2758C9.59842 19.3385 8.50905 18.8873 7.50485 19.2086V19.2086C5.83365 19.7433 4.25668 18.1663 4.79139 16.4951V16.4951C5.1127 15.491 4.66147 14.4016 3.7242 13.9187V13.9187C2.16438 13.1151 2.16438 10.8849 3.7242 10.0813V10.0813C4.66147 9.59842 5.1127 8.50905 4.79139 7.50485V7.50485C4.25668 5.83365 5.83365 4.25668 7.50485 4.79139V4.79139C8.50905 5.1127 9.59842 4.66147 10.0813 3.7242V3.7242Z" fill="currentColor"></path>
+                                        <path d="M14.8563 9.1903C15.0606 8.94984 15.3771 8.9385 15.6175 9.14289C15.858 9.34728 15.8229 9.66433 15.6185 9.9048L11.863 14.6558C11.6554 14.9001 11.2876 14.9258 11.048 14.7128L8.47656 12.4271C8.24068 12.2174 8.21944 11.8563 8.42911 11.6204C8.63877 11.3845 8.99996 11.3633 9.23583 11.5729L11.3706 13.4705L14.8563 9.1903Z" fill="white"></path>
+                                    </svg>
+                                </span>
+                                <!--end::Svg Icon-->
+                            </a>
+                        </div>
+                        <!--end::Name-->
+                        <div class="d-flex flex-wrap fw-semibold fs-6 mb-4 pe-2">
+                            <a href="#" class="d-flex align-items-center text-gray-400 text-hover-primary me-5 mb-2">
+                            <!--begin::Svg Icon | path: icons/duotune/communication/com006.svg-->
+                            <span class="svg-icon svg-icon-4 me-1">
+                                <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path opacity="0.3" d="M16.5 9C16.5 13.125 13.125 16.5 9 16.5C4.875 16.5 1.5 13.125 1.5 9C1.5 4.875 4.875 1.5 9 1.5C13.125 1.5 16.5 4.875 16.5 9Z" fill="currentColor"></path>
+                                    <path d="M9 16.5C10.95 16.5 12.75 15.75 14.025 14.55C13.425 12.675 11.4 11.25 9 11.25C6.6 11.25 4.57499 12.675 3.97499 14.55C5.24999 15.75 7.05 16.5 9 16.5Z" fill="currentColor"></path>
+                                    <rect x="7" y="6" width="4" height="4" rx="2" fill="currentColor"></rect>
+                                </svg>
+                            </span>
+                            <!--end::Svg Icon--><?= $orders->nama_pic ?></a>
+                            <a href="#" class="d-flex align-items-center text-gray-400 text-hover-primary me-5 mb-2">
+                            <!--begin::Svg Icon | path: icons/duotune/general/gen018.svg-->
+                            <span class="svg-icon svg-icon-4 me-1">
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path opacity="0.3" d="M18.0624 15.3453L13.1624 20.7453C12.5624 21.4453 11.5624 21.4453 10.9624 20.7453L6.06242 15.3453C4.56242 13.6453 3.76242 11.4453 4.06242 8.94534C4.56242 5.34534 7.46242 2.44534 11.0624 2.04534C15.8624 1.54534 19.9624 5.24534 19.9624 9.94534C20.0624 12.0453 19.2624 13.9453 18.0624 15.3453Z" fill="currentColor"></path>
+                                    <path d="M12.0624 13.0453C13.7193 13.0453 15.0624 11.7022 15.0624 10.0453C15.0624 8.38849 13.7193 7.04535 12.0624 7.04535C10.4056 7.04535 9.06241 8.38849 9.06241 10.0453C9.06241 11.7022 10.4056 13.0453 12.0624 13.0453Z" fill="currentColor"></path>
+                                </svg>
+                            </span>
+                            <!--end::Svg Icon--><?= bulan($orders->bulan) ?></a>
+                        </div>
+                    </div>
+                    <!--end::User-->
+                </div>
+                <!--end::Title-->
+
+                <!-- <div class="d-flex align-items-center w-200px w-sm-300px flex-column">
+                    <div class="d-flex justify-content-between w-100 mt-auto mb-2">
+                        <span class="fw-semibold fs-6 text-gray-400">Profile Compleation</span>
+                        <span class="fw-bold fs-6">50%</span>
+                    </div>
+                    <div class="h-5px mx-3 w-100 bg-light mb-3">
+                        <div class="bg-success rounded h-5px" role="progressbar" style="width: 50%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div> -->
+            </div>
+            <!--end::Info-->
+        </div>
+        <!--end::Details-->
+
         <!--begin::Nav-->
-        <ul class="nav nav-stretch nav-line-tabs nav-line-tabs-2x border-transparent fs-5 fw-bolder mt-n12">
+        <ul class="nav nav-stretch nav-line-tabs nav-line-tabs-2x border-transparent fs-5 fw-bolder mt-5">
             <!--begin::Nav item-->
             <li class="nav-item">
-                <a class="nav-link text-gray-600 py-5 me-6 active" data-bs-toggle="tab" href="#tab_konsumen">Data Order</a>
+                <a class="nav-link text-gray-600 py-5 me-6 active" data-bs-toggle="tab" href="#tab_order">Data Order</a>
             </li>
             <!--end::Nav item-->
             <!--begin::Nav item-->
             <li class="nav-item">
-                <a class="nav-link text-gray-600 py-5 me-6" data-bs-toggle="tab" href="#tab_crm">Data Produksi</a>
+                <a class="nav-link text-gray-600 py-5 me-6" data-bs-toggle="tab" href="#tab_produksi">Data Produksi</a>
             </li>
             <!--end::Nav item-->
             <!--begin::Nav item-->
@@ -30,5 +93,99 @@
             <!--end::Nav item-->
         </ul>
         <!--end::Nav-->
+
+        <div class="tab-content" id="myTabContent">
+            <div class="tab-pane fade show active" id="tab_order" role="tabpanel">
+                <div class="card mt-5">
+                    <div class="card-body">
+                        <h1 class="text-gray-900 fw-bolder fs-2">Data Order</h1>
+                        <h1 class="text-gray-500 fs-5 mt-2 mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora excepturi animi veniam ut eaque sapiente molestias, quasi nobis molestiae nostrum! Fugiat totam quod qui veritatis dolorem. Commodi necessitatibus nostrum dolorem.</h1>
+
+                        <div class="table-responsive">
+                            <table class="table table-borderless">
+                                <tr>
+                                    <th width="25%">Kode Order</th>
+                                    <th>: <?= $orders->kode_order ?></th>
+                                </tr>
+                                <tr>
+                                    <th width="25%">Tanggal Kontrak</th>
+                                    <th>: <?= longdate_indo($orders->tanggal) ?></th>
+                                </tr>
+                                <tr>
+                                    <th width="25%">Sumber</th>
+                                    <th>: <?= $orders->sumber ?></th>
+                                </tr>
+                                <tr>
+                                    <th width="25%">Nama Konsumen</th>
+                                    <th>: <?= $orders->nama_customer ?></th>
+                                </tr>
+                                <tr>
+                                    <th width="25%">No Hp</th>
+                                    <th>: <?= $orders->nohp ?></th>
+                                </tr>
+                                <tr>
+                                    <th width="25%">Status Order</th>
+                                    <th>: <?= $orders->deal_status ?></th>
+                                </tr>
+                                <tr>
+                                    <th width="25%">Status Follow Up</th>
+                                    <th>: <?= $orders->order_status ?></th>
+                                </tr>
+                                <tr>
+                                    <th width="25%">Instansi / Tipe Instansi</th>
+                                    <th>: <?= $orders->instansi." / (".$orders->jenis_instansi ?>)</th>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="tab-pane fade" id="tab_produksi" role="tabpanel">
+                <div class="card mt-5">
+                    <div class="card-body">
+                        <h1 class="text-gray-900 fw-bolder fs-2">Data Produksi</h1>
+                        <h1 class="text-gray-500 fs-5 mt-2 mb-5">Data Produksi adalah data lebih lanjut terkait informasi produksi.</h1>
+
+                        <div class="table-responsive">
+                            <table class="table table-borderless">
+                                <tr>
+                                    <th width="25%">Kode Order</th>
+                                    <th>: <?= $orders->kode_order ?></th>
+                                </tr>
+                                <tr>
+                                    <th width="25%">Tanggal Kontrak</th>
+                                    <th>: <?= longdate_indo($orders->tanggal) ?></th>
+                                </tr>
+                                <tr>
+                                    <th width="25%">Sumber</th>
+                                    <th>: <?= $orders->sumber ?></th>
+                                </tr>
+                                <tr>
+                                    <th width="25%">Nama Konsumen</th>
+                                    <th>: <?= $orders->nama_customer ?></th>
+                                </tr>
+                                <tr>
+                                    <th width="25%">No Hp</th>
+                                    <th>: <?= $orders->nohp ?></th>
+                                </tr>
+                                <tr>
+                                    <th width="25%">Status Order</th>
+                                    <th>: <?= $orders->deal_status ?></th>
+                                </tr>
+                                <tr>
+                                    <th width="25%">Status Follow Up</th>
+                                    <th>: <?= $orders->order_status ?></th>
+                                </tr>
+                                <tr>
+                                    <th width="25%">Instansi / Tipe Instansi</th>
+                                    <th>: <?= $orders->instansi." / (".$orders->jenis_instansi ?>)</th>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
